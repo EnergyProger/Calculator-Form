433c2a1d8ff68939db34029e8a69da9bfcbd0737
EnergyProger <edikmulyar228@gmail.com>
2021-07-06 16:23:48 +0300

    Calculator application, built with C# and Windows Forms

---
+++ Calculator_Forms/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator_Forms
+{
+    public partial class Form1 : Form
+    {
+        private string Action;
+        private string Number;
+        private bool Number2;
+        private Operations operation;
+
+        public Form1()
+        {
+            operation = new Operations();
+            Number2 = false;
+            InitializeComponent();
+        }
+
+        private void button22_Click(object sender, EventArgs e)
+        {
+            if (Number2)
+            {
+                Number2 = false;
+                textBox1.Text = "0";
+            }
+
+            Button B = (Button)sender;
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = B.Text;
+            }
+            else
+            {
+                textBox1.Text += B.Text;
+            }
+            
+
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "0";
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+            Button B = (Button)sender;
+            Action = B.Text;
+
+            Number = textBox1.Text;
+            Number2 = true;
+        }
+
+        private void button24_Click(object sender, EventArgs e)
+        {
+            double dn1, dn2;
+            double result = 0;
+
+            dn1 = Convert.ToDouble(Number);
+            dn2 = Convert.ToDouble(textBox1.Text);
+
+            if(Action == "+")
+            {
+                result = operation.Sum(dn1, dn2);
+            }
+            else if (Action == "-")
+            {
+                result = operation.Min(dn1, dn2);
+            }
+            else if (Action == "x")
+            {
+                result = operation.Mul(dn1, dn2);
+            }
+            else if (Action == "/")
+            {
+                result = operation.Del(dn1, dn2);
+            }
+            else if (Action == "%")
+            {
+                result = operation.Percent(dn1, dn2);
+            }
+
+            Action = "=";
+            Number2 = true;
+            textBox1.Text = result.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double dn = Convert.ToDouble(textBox1.Text);
+
+            textBox1.Text = operation.Sqrt(dn).ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double dn = Convert.ToDouble(textBox1.Text);
+
+            textBox1.Text = operation.Pow(dn).ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double dn = Convert.ToDouble(textBox1.Text);
+            double result = operation.OneDelNum(dn);
+
+            textBox1.Text = result.ToString();
+        }
+
+        private void button23_Click(object sender, EventArgs e)
+        {
+            double dn = Convert.ToDouble(textBox1.Text);
+            double result = operation.PlusMin(dn);
+
+            textBox1.Text = result.ToString();
+        }
+
+        private void button21_Click(object sender, EventArgs e)
+        {
+            if (!textBox1.Text.Contains(","))
+            {
+                textBox1.Text += ",";
+            }
+            
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+             textBox1.Text = textBox1.Text.Substring(0, textBox1.Text.Length-1);
+            if(textBox1.Text == "")
+            {
+                textBox1.Text = "0";
+            }
+        }
+    }
+}
---
+++ Calculator_Forms/Operations.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_Forms
+{
+    class Operations
+    {
+        public Operations() { }
+
+        public double Sum(double n1, double n2)
+        {
+            return n1 + n2;
+        }
+
+        public double Min(double n1, double n2)
+        {
+            return n1 - n2;
+        }
+
+        public double Mul(double n1, double n2)
+        {
+            return n1 * n2;
+        }
+
+        public double Del(double n1, double n2)
+        {
+            return n1 / n2;
+        }
+
+        public double Percent(double n1, double n2)
+        {
+            return n1 * n2 / 100;
+        }
+
+        public double Sqrt(double n)
+        {
+            return Math.Sqrt(n);
+        }
+
+        public double Pow(double n)
+        {
+            return Math.Pow(n,2);
+        }
+
+        public double OneDelNum(double n)
+        {
+            return 1 / n;
+        }
+
+        public double PlusMin(double n)
+        {
+            return -n;
+        }
+    }
+}
